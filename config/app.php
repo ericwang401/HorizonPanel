1e9acff7585b08738062e01efd1eb7140df9909a
SynEnt <37554696+SynEnt@users.noreply.github.com>
2020-04-28 18:03:25 -0500

    Syntax's commit | Added Argon Dashboard

--- config/app.php
+++ config/app.php
@@ -17,6 +17,19 @@ return [
 
     /*
     |--------------------------------------------------------------------------
+    | Application Logo
+    |--------------------------------------------------------------------------
+    |
+    | This value is the path to the logo of your application INSIDE your
+    | public folder. If your logo is in LARAVEL-APP/public/logo.png,
+    | please change it to logo.png.
+    |
+    */
+
+    'logo' => NULL,
+
+    /*
+    |--------------------------------------------------------------------------
     | Application Environment
     |--------------------------------------------------------------------------
     |
--- resources/views/auth/passwords/confirm.blade.php
+++ resources/views/auth/passwords/confirm.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.app')
+@extends('layouts.header')
 
 @section('content')
 <div class="container">
