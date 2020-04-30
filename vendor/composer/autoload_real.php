35838d4308e3793e9e43af4ef78c9aa6940cfb39
SynEnt <37554696+SynEnt@users.noreply.github.com>
2020-04-30 18:02:13 -0500

    Installed Laravel Passport | Created subscriptions model + contr. + migrations

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -13,9 +13,6 @@ class ComposerAutoloaderInitc8a1c469873cf7188955798a831e2f05
         }
     }
 
-    /**
-     * @return \Composer\Autoload\ClassLoader
-     */
     public static function getLoader()
     {
         if (null !== self::$loader) {
